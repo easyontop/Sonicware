2fdb6fae7030aa90a3551d6dac481d3bfb0376b9
KL-AE2160 <88426249+KL-AE2160@users.noreply.github.com>
2023-05-24 08:41:15 +0000

    regrege

--- Spec.lua
+++ Spec.lua
@@ -1,6 +1,7 @@
 local suc, dat = pcall(function()
   return loadstring(game:HttpGet("https://raw.githubusercontent.com/KL-AE2160/whitelist/main/list.json"))()
 end)
+repeat wait() until suc and dat ~= nil
 print("✔ | Loaded!", "Data: "..dat)
 local sx = game:GetService("HttpService")
 local xs = sx:JSONDecode(dat)
