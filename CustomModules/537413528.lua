c81b5b053083a612821dbe49d8661dc34de02cfd
KL-AE2160 <88426249+KL-AE2160@users.noreply.github.com>
2023-05-24 10:57:25 +0000

    Babft

---
+++ CustomModules/537413528.lua
@@ -0,0 +1,5 @@
+repeat task.wait() until game:IsLoaded()
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7granddadpgn/vapev4forroblox/main/NewMainScript.lua", true))()
+  end)
