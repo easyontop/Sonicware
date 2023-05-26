c36d47d7993b5b30c5d6d1e6cadfdf49e86c92f4
easyontop <88426249+easyontop@users.noreply.github.com>
2023-05-26 12:18:10 +0000

    t3

--- CustomModules/6872265039.lua
+++ CustomModules/6872265039.lua
@@ -1,5 +1,7 @@
+repeat task.wait() until game:IsLoaded()
 pcall(
   function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
   end
 )
