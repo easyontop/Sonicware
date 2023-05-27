a87cb95f1d533701f4a06b86559e989d923b3f54
easyontop <88426249+easyontop@users.noreply.github.com>
2023-05-27 06:34:10 +0000

    L bedwars hackers - Sonicware discontinued for it

--- CustomModules/6872265039.lua
+++
@@ -1,7 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-pcall(
-  function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
-  end
-)
--- CustomModules/8444591321.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/easyontop/Sonicware/main/CustomModules/6872274481.lua"))()
