3a0b218d41afda2f9316c3c1cf551939638ef145
easyontop <88426249+easyontop@users.noreply.github.com>
2023-05-31 16:19:59 +0000

    xe

--- CustomModules/11454403177.lua
+++ CustomModules/11454403177.lua
@@ -1,7 +1,3 @@
 -- Cart Ride Around Nothing
 loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))()
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Loaded!";
-    Text = "Use ;vfly | ;unvfly to make the cart fly!";
-    Duration = 10;
-})
+shared.GUI:CreateNotification("info", "Use ;vfly | ;unvfly to make cart fly.", 15)
