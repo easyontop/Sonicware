b05d532c5d192bb97381c05d8c686bd239a4a4ed
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-04 05:36:11 +0000

    Fix2

--- CustomModules/7449423635.lua
+++ CustomModules/7449423635.lua
@@ -6,11 +6,11 @@ local lplr = playersService.LocalPlayer
 replicatedStorageService.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(
   function(tab, channel)
     local plr = playersService:FindFirstChild(tab.FromSpeaker)
-    local name = plr ~= nil and "["..plr.Name.."]: " or "[System]: "
+    local name = plr ~= nil and "["..plr ~= lplr and plr.Name or "You".."]: " or "[System]: "
     local msg = tab.Message
     shared.GUI:CreateNotification(
       "info",
-      name..msg,
+      (tab.MessageType == "Whisper" and "DM: "..name or name)..msg,
       string.len(msg)*0.2 > 5 and string.len(msg)*0.2 or 5
     )
   end
