9356b0f33ccdde2a83d6697d3008f192d81f096d
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-04 06:01:27 +0000

    PlrAdded&PlrLeaving

--- CustomModules/7449423635.lua
+++ CustomModules/7449423635.lua
@@ -6,12 +6,24 @@ local lplr = playersService.LocalPlayer
 replicatedStorageService.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(
   function(tab, channel)
     local plr = playersService:FindFirstChild(tab.FromSpeaker)
-    local name = plr ~= nil and "["..plr.Name.."]: " or "[System]: "
+    local name = plr ~= nil and "<font color='rgb(0, 255, 0)'>["..plr.Name.."]</font>: " or "<font color='rgb(255, 0, 0)'>[System]</font>: "
     local msg = tab.Message
     shared.GUI:CreateNotification(
       "info",
       name..msg,
       string.len(msg)*0.2 > 5 and string.len(msg)*0.2 or 5
     )
+  end
+)
+playersService.PlayerAdded:Connect(
+  function(plr)
+    if not plr then return end
+    shared.GUI:CreateNotification("info", plr.Name.." has joined the experience.", 10)
+  end
+)
+
+playersService.PlayerRemoving:Connect(
+  function(plr)
+    shared.GUI:CreateNotification("info", plr.Name.." has left the experience.", 10)
   end
 )
