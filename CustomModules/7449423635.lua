4ab6711e59f55db738a926c3710e3e7ac5c29ccd
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-04 09:39:18 +0000

    now for all games lol

--- CustomModules/7449423635.lua
+++ CustomModules/7449423635.lua
@@ -1,29 +1,2 @@
 shared.GUI:CreateNotification("success", "Loading Zamex Hub - Sonicware", 5)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Sixnumz/ZamexMobile/main/Zamex_Mobile.lua'))() 
-local replicatedStorageService = game:GetService("ReplicatedStorage")
-local playersService = game:GetService("Players")
-local lplr = playersService.LocalPlayer
-replicatedStorageService.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(
-  function(tab, channel)
-    local plr = playersService:FindFirstChild(tab.FromSpeaker)
-    local name = plr ~= nil and "<font color='rgb(0, 255, 0)'>["..plr.Name.."]</font>: " or "<font color='rgb(255, 0, 0)'>[System]</font>: "
-    local msg = tab.Message
-    shared.GUI:CreateNotification(
-      "info",
-      name..msg,
-      string.len(msg)*0.2 > 5 and string.len(msg)*0.2 or 5
-    )
-  end
-)
-playersService.PlayerAdded:Connect(
-  function(plr)
-    if not plr then return end
-    shared.GUI:CreateNotification("info", plr.Name.." has joined the experience.", 10)
-  end
-)
-
-playersService.PlayerRemoving:Connect(
-  function(plr)
-    shared.GUI:CreateNotification("info", plr.Name.." has left the experience.", 10)
-  end
-)
--- Execute.lua
+++ Execute.lua
@@ -54,6 +54,33 @@ local suc, res = pcall(function()
 end)
 if not suc or res == "404: Not Found" then return shared.GUI:CreateNotification("warning", "No Scripts are avaliable for this game. Sorry for the inconvenience.", 20) end
 shared.GUI:CreateNotification("info", "Loading Scripts...", 4)
+local replicatedStorageService = game:GetService("ReplicatedStorage")
+local playersService = game:GetService("Players")
+local lplr = playersService.LocalPlayer
+replicatedStorageService.DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(
+  function(tab, channel)
+    local plr = playersService:FindFirstChild(tab.FromSpeaker)
+    local name = plr ~= nil and "<font color='rgb(0, 255, 0)'>["..plr.Name.."]</font>: " or "<font color='rgb(255, 0, 0)'>[System]</font>: "
+    local msg = tab.Message
+    shared.GUI:CreateNotification(
+      "info",
+      name..msg,
+      string.len(msg)*0.2 > 5 and string.len(msg)*0.2 or 5
+    )
+  end
+)
+playersService.PlayerAdded:Connect(
+  function(plr)
+    if not plr then return end
+    shared.GUI:CreateNotification("info", plr.Name.." has joined the experience.", 10)
+  end
+)
+
+playersService.PlayerRemoving:Connect(
+  function(plr)
+    shared.GUI:CreateNotification("info", plr.Name.." has left the experience.", 10)
+  end
+)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/easyontop/Sonicware/main/CustomModules/"..game.PlaceId..".lua"))()
 shared.GUI:CreateNotification("success", "Sonicware Loaded :D", 3)
 local RainbowLogo = Instance.new("ScreenGui")
