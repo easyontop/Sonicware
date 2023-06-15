e442a2de9d12320af9cb2c33b7df0672f177eb09
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-15 05:02:06 +0000

    why02

--- CustomModules/12215278065.lua
+++
@@ -1,6 +0,0 @@
--- Toy Solider Z
-local playersService = game:GetService("Players")
-local lplr = playersService.LocalPlayer
-local replicatedStorageService = game:GetService("ReplicatedStorage")
-repeat task.wait() until game:IsLoaded()
-local GuiLibrary = shared.GUI
