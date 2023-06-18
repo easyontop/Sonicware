2d9ef69dcf01cb2294d1259d9a0182c558144df9
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-18 09:33:04 +0000

    we

--- CustomModules/12215278065.lua
+++ CustomModules/12215278065.lua
@@ -16,34 +16,10 @@ function bxp(x)
       a.Size = x.Size
       a.Color = BrickColor.new("Lime green")
       a.Transparency = .6
-      local b = Instance.new("BillboardGui")
-			local c = Instance.new("TextLabel")
-      b.Adornee = x
-      b.Parent = x
-      b.Name = "tedtroll"
-      b.AlwaysOnTop = true
-      b.ZIndex = 0
-      b.Size = UDim2.new(0,150,0,200)
-      b.Transparency = 
-      b.Color = BrickColor.new("Lime green")
-      b.StudsOffset = Vector3.new(0, 1, 0)
-      c.Parent = b
-      c.Name = "ted"
-      c.BackgroundTransparency = 1
-      c.Position = UDim2.new(0, 0, 0, -50)
-			c.Size = UDim2.new(0, 150, 0, 150)
-			c.Font = Enum.Font.Gotham 
-      c.TextColor3 = Color3.fromRGB(255, 255, 255)
-      c.TextSize = 30
-      c.TextStrokeTransparency = 0
-			c.TextYAlignment = Enum.TextYAlignment.Bottom
-			c.Text = "🐻 Pick me up!"
-		  c.ZIndex = 10
     end
   )
 end
-
 for i, v in pairs(workspace:GetDescendants()) do
-  bxp(v)
+  task.spawn(function() bxp(v) end)
 end
-workspace.DescendantAdded:Connect(function(v) bxp(v) end)
+workspace.DescendantAdded:Connect(function(v) task.spawn(function() bxp(v) end) end)
