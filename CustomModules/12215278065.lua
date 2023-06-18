5c50bad18889b40490cdba576b9219752b239341
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-18 09:29:04 +0000

    kwkn

--- CustomModules/12215278065.lua
+++ CustomModules/12215278065.lua
@@ -1,34 +1,25 @@
 -- Toy Solider Z
+repeat task.wait() until game:IsLoaded()
 local playersService = game:GetService("Players")
 local lplr = playersService.LocalPlayer
 local replicatedStorageService = game:GetService("ReplicatedStorage")
-repeat task.wait() until game:IsLoaded()
-local GuiLibrary = shared.GUI
-local char = lplr.Character
-local team = {
-  Cyan = "Blue",
-  Moss = "Green",
-  Daisy = "Yellow",
-  Persimmon = "Red"
-}
-
 function bxp(x)
   pcall(
     function()
       if not x.Parent or x.Parent.Name ~= "teddy" then return end
-      local a = Instance.new("BoxHandleAdornment", x)
+      local a = Instance.new("BoxHandleAdornment")
       a.Name = "TED_PESP"
-      --a.Parent = x
+      a.Parent = x
       a.Adornee = x
       a.AlwaysOnTop = true
       a.ZIndex = 0
       a.Size = x.Size
       a.Color = BrickColor.new("Lime green")
       a.Transparency = .6
-      local b = Instance.new("BillboardGui", x)
-			local c = Instance.new("TextLabel", b)
+      local b = Instance.new("BillboardGui")
+			local c = Instance.new("TextLabel")
       b.Adornee = x
-      --b.Parent = x
+      b.Parent = x
       b.Name = "tedtroll"
       b.AlwaysOnTop = true
       b.ZIndex = 0
@@ -36,7 +27,7 @@ function bxp(x)
       b.Transparency = 
       b.Color = BrickColor.new("Lime green")
       b.StudsOffset = Vector3.new(0, 1, 0)
-      --c.Parent = b
+      c.Parent = b
       c.Name = "ted"
       c.BackgroundTransparency = 1
       c.Position = UDim2.new(0, 0, 0, -50)
