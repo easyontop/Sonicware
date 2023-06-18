b1a033624c2b1e8533651ec533f5a48dd4ea18db
easyontop <88426249+easyontop@users.noreply.github.com>
2023-06-18 15:30:33 +0000

    Edit [M]

--- CustomModules/11708967881.lua
+++ CustomModules/11708967881.lua
@@ -29,28 +29,3 @@ GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"]["CreateOptionsButton
     end
   }
 )
-local function TP(POS)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = POS
-end
-_G["at"] = false
-GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"]["CreateOptionsButton"](
-  {
-    ["Enabled"] = false,
-    ["Name"] = "AutoThrow",
-    ["Function"] = function(cb)
-      _G["at"] = cb
-      while _G["at"] do
-        pcall(
-          function() 
-            TP(CFrame.new(-94.4807968, 281.926392, -110.089729, -0.0431109145, 4.41078996e-08, -0.999070287, 2.23052012e-08, 1, 4.31864535e-08, 0.999070287, -2.04226573e-08, -0.0431109145))
-            wait(0.1)
-            game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Throw"):WaitForChild("Server"):WaitForChild("Request"):FireServer()
-            wait(1)
-            TP(CFrame.new(9667.20312, 194.237198, -107.84156, -0.000386250351, 0.187470704, -0.982270122, 1.00458779e-08, 0.982270181, 0.187470719, 0.99999994, 7.24007623e-05, -0.000379404082))
-            wait(7)
-          end
-        )
-      end
-    end
-  }
-)
